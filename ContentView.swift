6c5da244672301ac86c60d28e63062adba61b91d
Ola Oleksiak <aoleksiak8215@stu.d214.org>
2025-04-04 11:15:21 -0500

    updated version

--- ContentView.swift
+++ ContentView.swift
@@ -4,5 +4,13 @@ struct ContentView: View {
     var body: some View {
        Text("OLA")
         Text("sara")
+        
+        Image(systemName: "heart")
+        RoundedRectangle(cornerRadius: 0.9)
+            .size(CGSize(width: 100, height: 100))
+            
+        Image(systemName: "heart")
+        RoundedRectangle(cornerRadius: 0.9)
+            .size(CGSize(width: 100, height: 100))
     }
 }
